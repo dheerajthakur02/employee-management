deaa3158547acf720cd4024b3b1db7a1c66631d5
Dheeraj Kumar Thakur <dk35118@gmail.com>
2025-06-27 11:11:44 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDB = async()=>{
+       try {
+           await mongoose.connect(process.env.MONGODB_URL_LOCAL)
+           console.log("Database connected successfully")
+       } catch (error) {
+           console.log(error)
+       }
+} 
---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+import { connectDB } from "./db.js";
+dotenv.config();
+//To handle the routes
+const app = express();
+const PORT= process.env.PORT || 3000
+connectDB();
+
+
+
+
+app.listen(3000,()=>{
+     console.log(`Server is running on port ${PORT}`)
+})
